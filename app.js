6bbec8f1cdbe89bc1ff9505c303f7a2baedfd005
Vikram Aditya <157366925+vikram-a2004@users.noreply.github.com>
2024-03-29 17:19:58 +0530

    Update app.js
    certain Change happened

--- app.js
+++ app.js
@@ -25,10 +25,6 @@ const initializiDbServer = async () => {
 }
 initializiDbServer()
 
-/*const logger = (request, respponse, next) => {
-  console.log(request.query)
-  next()
-}*/
 //convert to Response Object
 const convertResponseObject = dbObject => {
   return {
@@ -38,32 +34,32 @@ const convertResponseObject = dbObject => {
   }
 }
 // authenticationToken
+
 const authenticationToken = (request, response, next) => {
   let jwtToken
-  const authHeadther = request.headers['authorization']
-  if (authHeadther !== undefined) {
-    jwtToken = authHeadther.split(' ')[1]
+  const authHeader = request.headers['authorization']
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1]
   }
-
   if (jwtToken === undefined) {
     response.status(401)
     response.send('Invalid JWT Token')
   } else {
-    jwt.verify(jwtToken, 'secreat_key', async (error, payload) => {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
       if (error) {
         response.status(401)
-        response.send('Invalid jwtToken')
+        response.send('Invalid JWT Token')
       } else {
-        //console.log(payload)
-        // request.username = payload.username
+        //request.username = payload.username
         next()
       }
     })
   }
 }
 
-// get all district
-app.get('/states', authenticationToken, async (request, response) => {
+//API-2
+// get all State
+app.get('/states', async (request, response) => {
   //console.log('Get State  APIs')
   // verifying jwtToken
   const selectDistrictQuery = `SELECT * FROM state;`
@@ -71,7 +67,7 @@ app.get('/states', authenticationToken, async (request, response) => {
   response.send(distrctData.map(convertResponseObject))
 })
 // Register user Id
-app.post('/user/', authenticationToken, async (request, response) => {
+app.post('/user/', async (request, response) => {
   const {username, name, password, gender, location} = request.body
   const hasshedPassword = await bcrypt.hash(password, 10)
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`
@@ -91,7 +87,7 @@ app.post('/user/', authenticationToken, async (request, response) => {
     ])
     response.send('User Created Successfully')
   } else {
-    response.status = 400
+    response.status(400)
     response.send('User already exits')
   }
 })
@@ -109,7 +105,7 @@ app.post('/login', async (request, response) => {
       //response.send('Login Success!')
       // Generating jwts Token
       const payload = {username: username}
-      const jwtToken = jwt.sign(payload, 'secreat_key')
+      const jwtToken = jwt.sign(payload, 'MY_SECRET_TOKEN')
       response.send({jwtToken})
     } else {
       response.status(400)
@@ -118,9 +114,9 @@ app.post('/login', async (request, response) => {
   }
 })
 
-//GET Method APIs
+//GET Method APIs -3
 //Returns a state based on the state ID
-app.get('/states/:stateId/', async (request, response) => {
+app.get('/states/:stateId/', authenticationToken, async (request, response) => {
   const {stateId} = request.params
   const getSelectQuery = `SELECT * FROM state WHERE state_id =?;`
   const getDataById = await db.get(getSelectQuery, [stateId])
@@ -140,7 +136,7 @@ const convertDistrictResponseObject = dbObject => {
   }
 }
 // Post Create an districti in district table
-app.post('/districts', async (request, response) => {
+app.post('/districts', authenticationToken, async (request, response) => {
   const {districtName, stateId, cases, cured, active, deaths} = request.body
   const postDetailsQuery = `
   INSERT INTO district (district_name, state_id, cases, cured, active, deaths)
@@ -173,57 +169,69 @@ const convertDBResponseObject1 = dbObject => {
   }
 }
 
-app.get('/districts/:districtId/', async (request, response) => {
-  const {districtId} = request.params
-  const getdistrictByIdQuery = `
+app.get(
+  '/districts/:districtId/',
+  authenticationToken,
+  async (request, response) => {
+    const {districtId} = request.params
+    const getdistrictByIdQuery = `
   SELECT * FROM district WHERE district_id= ?;
   `
-  const getDistrictId = await db.get(getdistrictByIdQuery, [districtId])
-  response.send(convertDBResponseObject1(getDistrictId))
-})
+    const getDistrictId = await db.get(getdistrictByIdQuery, [districtId])
+    response.send(convertDBResponseObject1(getDistrictId))
+  }
+)
 
 // API-6 DELETE
 //Deletes a district from the district table based on the district ID
 
-app.delete('/districts/:districtId', async (request, response) => {
-  const {districtId} = request.params
-  const deleteQuery = `
+app.delete(
+  '/districts/:districtId',
+  authenticationToken,
+  async (request, response) => {
+    const {districtId} = request.params
+    const deleteQuery = `
   DELETE FROM district WHERE district_id = ?;
   `
-  const deleteUpdate = await db.run(deleteQuery, [districtId])
-  const updateQuery = convertResponseObject(deleteUpdate)
-  response.send('District Removed')
-})
+    const deleteUpdate = await db.run(deleteQuery, [districtId])
+    const updateQuery = convertResponseObject(deleteUpdate)
+    response.send('District Removed')
+  }
+)
 
 //API-7 PUT
 //Updates the details of a specific district based on the district ID
-app.put('/districts/:districtId/', async (request, response) => {
-  const {districtId} = request.params
-  const {district_name, state_id, cases, cured, active, deaths} = request.body
+app.put(
+  '/districts/:districtId/',
+  authenticationToken,
+  async (request, response) => {
+    const {districtId} = request.params
+    const {districtName, stateId, cases, cured, active, deaths} = request.body
 
-  const updateQuery = `
+    const updateQuery = `
     UPDATE district
-    SET 
+    SET (
       district_name = ?, 
       state_id = ?, 
       cases = ?, 
       cured = ?, 
       active = ?, 
-      deaths = ? 
+      deaths = ? )
     WHERE district_id=?;
   `
-  const getUpdatedId = await db.run(updateQuery, [
-    district_name,
-    state_id,
-    cases,
-    cured,
-    active,
-    deaths,
-    districtId,
-  ])
-  const convertedDistrict = specificDistrict(getUpdatedId)
-  response.send('District Details Updated')
-})
+    await db.run(updateQuery, [
+      districtName,
+      stateId,
+      cases,
+      cured,
+      active,
+      deaths,
+      districtId,
+    ])
+    //const convertedDistrict = specificDistrict(getUpdatedId)
+    response.send('District Details Updated')
+  }
+)
 
 //API-8 get
 //Returns the statistics of total cases, cured, active, deaths of a specific state based on state ID
@@ -235,9 +243,12 @@ const statisticsOfTatolCases = dbObject => {
     totalDeaths: dbObject.total_deaths,
   }
 }
-app.get('/states/:stateId/stats/', async (request, response) => {
-  const {stateId} = request.params
-  const sumOfTotalQuery = `
+app.get(
+  '/states/:stateId/stats/',
+  authenticationToken,
+  async (request, response) => {
+    const {stateId} = request.params
+    const sumOfTotalQuery = `
   SELECT 
     SUM(cases) as total_cases,
     SUM(cured) as total_cured,
@@ -248,8 +259,9 @@ app.get('/states/:stateId/stats/', async (request, response) => {
   WHERE 
     state_id =?
   `
-  const getsumData = await db.get(sumOfTotalQuery, [stateId])
-  response.send(statisticsOfTatolCases(getsumData))
-})
+    const getsumData = await db.get(sumOfTotalQuery, [stateId])
+    response.send(statisticsOfTatolCases(getsumData))
+  }
+)
 
 module.exports = app
